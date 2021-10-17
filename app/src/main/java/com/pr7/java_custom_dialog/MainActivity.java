a91f9e44c30df903ff540fae1f4017b6b0313581
Fourcade7 <ccm8d77777>
2021-10-17 20:59:25 +0500

    Initial commit

---
+++ app/src/main/java/com/pr7/java_custom_dialog/MainActivity.java
@@ -0,0 +1,32 @@
+package com.pr7.java_custom_dialog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void show(View view) {
+        View view1= LayoutInflater.from(MainActivity.this).inflate(R.layout.dialog_lay,null);
+
+
+        Dialog dialog=new Dialog(MainActivity.this);
+        dialog.setContentView(view1);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().setLayout(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        dialog.create();
+        dialog.show();
+    }
+}
